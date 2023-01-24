19418294edf4895cb2323adbf797910bb7e5a8e8
Ayman Rahman <ayman2002rahman@gmail.com>
2023-01-24 16:41:07 -0600

    Close to being done but when a cell is collapsed due to the propgation of another cell, it doesn't propgate (chain reaction not happening)

--- Cell.js
+++ Cell.js
@@ -1,15 +1,89 @@
+const CELL_SIZE = 70;
+const SELECTOR_SPACING = 15;
+
 class Cell {
-    constructor() {
-        this.possible = Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+
+        this.possible = [];
+        let num = 1;
+        for(let i = 0; i < 3; i++) {
+            let tempArr = [];
+            for(let j = 0; j < 3; j++) {
+                tempArr.push(new Selector(SELECTOR_SPACING + this.x + i * SELECTOR_SPACING, SELECTOR_SPACING + this.y + j * SELECTOR_SPACING, num));
+                num++;
+            }
+            this.possible.push(tempArr);
+        }
         this.entropy = 10;
+
+        this.collapsed = false;
+        this.num = -1;
+    }
+
+    isClicked() {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                if(this.possible[i][j].isClicked() && this.possible[i][j].isActive()) { //This returns a boolean so more must be done with this info
+                    this.collapse(this.possible[i][j].getNum());
+                    //Now we need to remove that number as a possiblity in its respected row, column, a nd 3x3 cell
+                    return this.possible[i][j].getNum();
+                }
+            }
+        }
+        return -1;
     }
 
     collapse(state) {
-        this.possible = Set([state]);
-        this.entropy = 0;
+        if(!this.collapsed) {
+            this.collapsed = true;
+            this.num = state;
+
+            
+        }
+    }
+
+    update() {
+        //2D array for possible states may not be efficient because I would need to loop through it everytime to figure out which selector is the only one active
+        let count = 0;
+        let num = -1;
+        for(let i = 0; i < this.possible.length; i++)
+            for(let j = 0; j < this.possible[0].length; j++)
+                if(this.possible[i][j].isActive()) {
+                    num = this.possible[i][j].getNum();
+                    count++;
+                }
+        if(count == 1) {
+            this.collapse(num);
+        }
+    }
+
+    removePossibleState(num) {
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                if(this.possible[i][j].getNum() == num) {
+                    this.possible[i][j].makeInactive();
+                    return;
+                }
+            }
+        }
     }
 
     display() {
-        
+        if(!this.collapsed) {
+            fill(100, 100, 100);
+            rect(this.x, this.y, CELL_SIZE, CELL_SIZE);
+            for(let i = 0; i < 3; i++)
+                for(let j = 0; j < 3; j++)
+                    this.possible[i][j].display();
+        }
+        else {
+            fill('blue');
+            rect(this.x, this.y, CELL_SIZE, CELL_SIZE);
+            fill('white');
+            textSize(30);
+            text(this.num, this.x + 27, this.y + 45);
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,107 @@
+const CANVAS_SIZE = 700;
+const X_OFFSET = 30;
+const Y_OFFSET = 30;
+
+let board = [];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(CANVAS_SIZE, CANVAS_SIZE);
+  for(let i = 0; i < 9; i++) {
+    row = [];
+    for(let j = 0; j < 9; j++)
+      row.push(new Cell(j * CELL_SIZE + X_OFFSET, i * CELL_SIZE + Y_OFFSET));
+    board.push(row);
+  }
 }
 
+function mousePressed() {
+  for(let i = 0; i < 9; i++) {
+    for(let j = 0; j < 9; j++) {
+      let num = board[i][j].isClicked();
+      if(num != -1) {
+        for(let k = 0; k < 9; k++) {
+          board[k][j].removePossibleState(num);
+          board[i][k].removePossibleState(num);
+        }
+        let bigI = Math.floor(i / 3);
+        let bigJ = Math.floor(j / 3);
+        switch(bigI) {
+          case 0:
+            switch(bigJ) {
+              case 0: // (0, 0)
+                for(let k = 0; k < 3; k++)
+                  for(let l = 0; l < 3; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+              case 1: // (0, 1)
+                for(let k = 0; k < 3; k++)
+                  for(let l = 3; l < 6; l++)
+                    board[k][l].removePossibleState(num);              
+                break;
+              case 2: //(0, 2)
+                for(let k = 0; k < 3; k++)
+                  for(let l = 6; l < 9; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+            }
+            break;
+          case 1:
+            switch(bigJ) {
+              case 0: //(1, 0)
+                for(let k = 3; k < 6; k++)
+                  for(let l = 0; l < 3; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+              case 1: //(1, 1)
+                for(let k = 3; k < 6; k++)
+                  for(let l = 3; l < 6; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+              case 2: //(1, 2)
+                for(let k = 3; k < 6; k++)
+                  for(let l = 6; l < 9; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+            }
+            break;
+          case 2:
+            switch(bigJ) {
+              case 0: //(2, 0)
+                for(let k = 6; k < 9; k++)
+                  for(let l = 0; l < 3; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+              case 1: //(2, 1)
+                for(let k = 6; k < 9; k++)
+                  for(let l = 3; l < 6; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+              case 2: //(2, 2)
+                for(let k = 6; k < 9; k++)
+                  for(let l = 6; l < 9; l++)
+                    board[k][l].removePossibleState(num);
+                break;
+            }
+            break;
+        }
+      }
+    }
+  }
+  for(let i = 0; i < 9; i++)
+    for(let j = 0; j < 9; j++)
+      board[i][j].update();
+}
+
+//This function is on an infinite loop at 60 fps
 function draw() {
-  background('blue');
-  //rect(100, 100, 50, 50);C
+  background('grey');
+  stroke('black')
+  strokeWeight(1);
+  for(let i = 0; i < board.length; i++)
+    for(let j = 0; j < board.length; j++)
+      board[i][j].display();
+  strokeWeight(5);
+  for(let i = 3; i <= 6; i += 3)
+    line(i * CELL_SIZE + X_OFFSET, Y_OFFSET, i * CELL_SIZE + X_OFFSET, 9 * CELL_SIZE + Y_OFFSET);
+  for(let i = 3; i <= 6; i+= 3)
+    line(X_OFFSET, i * CELL_SIZE + Y_OFFSET, 9 * CELL_SIZE + X_OFFSET, i * CELL_SIZE + Y_OFFSET);
 }
